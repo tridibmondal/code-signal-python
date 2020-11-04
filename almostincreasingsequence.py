e52e07f5b32dc4a2981f05cf81c9e4dd9611df89
tridibmondal <tlucifer2000@gmail.com>
2020-11-05 01:28:24 +0530

    almost increasing

---
+++ almostincreasingsequence.py
@@ -0,0 +1,22 @@
+https://app.codesignal.com/arcade/intro/level-2/2mxbGwLzvkTCKAJMG
+
+
+solution
+
+
+def almostIncreasingSequence(sequence):
+    droppped = False
+    last = prev = min(sequence) - 1
+    for elm in sequence:
+        if elm <= last:
+            if droppped:
+                return False
+            else:
+                droppped = True
+            if elm <= prev:
+                prev = last
+            elif elm >= prev:
+                prev = last = elm
+        else:
+            prev, last = last, elm
+    return True
